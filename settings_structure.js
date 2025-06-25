d1916956cb2f7bfc56db93d781cffa8a04bcbf75
DragonTera <75659889+DragonTera@users.noreply.github.com>
2025-06-25 17:07:43 -0300

    Add files via upload

---
+++ settings_structure.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        "key": "ENABLE",
+        "name": "Enable mod:",
+        "type": "bool"
+    },
+    {
+        "key": "FORCE_PUNISHING_BLOW",
+        "name": "Force Punishing Blow before Measured Slice:",
+        "type": "bool"
+    },
+    {
+        "key": "AUTO_MEASURED_SLICE",
+        "name": "Auto Measured Slice after force Punishing Blow:",
+        "type": "bool"
+    },
+    {
+        "key": "FORCE_HEADLONG_RUSH",
+        "name": "Force Headlong Rush before Whirlwind:",
+        "type": "bool"
+    },
+    {
+        "key": "OVERPOWER_NOTIFY",
+        "name": "Notify if Overpower is not active:",
+        "type": "bool"
+    }
+];
